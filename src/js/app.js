91469568c45c514cac6a4a8f42cd2ccd74fd44f0
Ryan <ryan@commercialprogression.com>
2018-09-27 16:13:23 -0400

    recipe layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,11 @@
 // JS Goes here - ES6 supported
 
-// Attempt to grab user.
-const user = auth.currentUser();
-
-// Netlify CMS.
+// Netlify Identity.
 if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", (user) => {
     if (!user) {
       window.netlifyIdentity.on("login", () => {
-        document.location.href = "/";
+        document.location.href = "/admin/";
       });
     }
   });
